46f9a8b427a98b34d41fb63f1a53cce708895860
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-12-11 18:28:33 +0100

    fix some sizing to navbar main logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { Link, NavLink } from "react-router-dom";
 export default function Navbar(){
   return(
     <div className="d-flex justify-content-between bg-light container-fluid py-2 px-4">
-      <div className="brand">
-        <h3>Movies</h3>
-      </div>
+
+        <Link to={"/"}><h3 className="m-0">Movies</h3></Link>
+
       <div className="links">
         <ul className="list-unstyled list-group">
           <NavLink to="/"><li className="list-group-item">Home</li></NavLink>
