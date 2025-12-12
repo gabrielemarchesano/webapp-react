870f15c3408992903a8983df86ab76dc39293cc5
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-12-12 12:33:25 +0100

    style single movie page

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -17,8 +17,36 @@ export default function MoviePage(){
   }, []);
 
   return(
-    <>
-      <h1>{movie.title}</h1>
-    </>
+    <div className="container">  
+      <div className="d-flex justify-content-center">
+      
+      <div className="card movie-card my-5">
+        <div className="row">
+          <div className="col-4">
+            <img src={`http://localhost:3000/${movie.image}`} alt="" className="card-img-top"/>
+          </div>
+          <div className="col-8">
+            <div className="card-body">
+              <h2 className="card-title">{movie.title}</h2>
+              <p className="card-text">{movie.abstract}</p>
+              <small>{movie.director} - {movie.release_year} - {movie.genre}</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    <div className="reviews">
+        {
+          movie.reviews.map((review) => (
+            <div className="card reviews-card">
+              <span>{review.vote} <i class="bi bi-star-fill"/> - {review.name}</span>
+              <div className="card-body">
+                <p className="card-text">"{review.text}"</p>
+              </div>
+            </div>
+          ))
+        }
+    </div>
+    </div>
   )
 }
